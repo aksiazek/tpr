71cf5ec569c168367f4627390a913a7c02dc04e3
aksiazek <alexander.ksiazek@gmail.com>
2015-05-19 10:18:52 +0200

    addVectors time measurement

--- cuda/addVectors.cu
+++ cuda/addVectors.cu
@@ -11,10 +11,14 @@ __global__ void add (int *a, int *b, int *c) {
 }
 int main(void) {
     int a[N],b[N],c[N];
-    int *dev_a, *dev_b, *dev_c;
+    // float a_h[N];
+    int *dev_a, *dev_b, *dev_c;//, *a_d;
     cudaMalloc((void**)&dev_a, N * sizeof(int));
     cudaMalloc((void**)&dev_b, N * sizeof(int));
     cudaMalloc((void**)&dev_c, N * sizeof(int));
+    
+    // cudaMalloc((void **) &a_d, sizeof(float)*N); // alokuj pamięć na GPU
+    
     for (int i=0; i<N; i++) {
         a[i] = i;
         b[i] = i*2;
@@ -22,11 +26,29 @@ int main(void) {
     cudaMemcpy(dev_a, a, N*sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_b, b, N*sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_c, c, N*sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(a_d, a_h, sizeof(float)*N, cudaMemcpyHostToDevice);
+    
+    StopWatchInterface *timer=NULL;
+    sdkCreateTimer(&timer);
+    sdkResetTimer(&timer);
+    sdkStartTimer(&timer);
+    
     add <<<1,N>>> (dev_a,dev_b,dev_c);
+    
+    cudaThreadSynchronize();
+    sdkStopTimer(&timer);
+    float time = sdkGetTimerValue(&timer);
+    sdkDeleteTimer(&timer);
+    
     cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);
     for (int i = 0; i < N; i++) {
         printf("%d+%d=%d\n", a[i], b[i], c[i]);
     }
+    
+    // cudaMemcpy(a_h, a_d, sizeof(float)*N, cudaMemcpyDeviceToHost);
+    printf ("Time for the kernel: %f ms\n", time);
+    
+    // cudaFree(a_d);
     cudaFree(dev_a);
     cudaFree(dev_b);
     cudaFree(dev_c);
